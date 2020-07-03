e52085c8f95aa9a8c88fc97e8e734a6be77f7c07
Dhanasekaran Subramani <dhanasekaran.subramani@rabobank.nl>
2020-07-03 14:17:27 +0530

    Springboot project initial setup

---
+++ spring-boot-features-security-ssl-https/src/main/java/com/ds2/springboot/security/HomeController.java
@@ -0,0 +1,13 @@
+package com.ds2.springboot.security;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/api")
+    public String welcome(){
+        return "Hello, World";
+    }
+}
---
+++ spring-boot-features-security-ssl-https/src/test/java/com/ds2/springboot/security/SecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ds2.springboot.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
