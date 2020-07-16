0bcc93756a2a4d58b54e19a8ef33e08a7dd66a67
Dhanasekaran Subramani <dhanasekaran.subramani@rabobank.nl>
2020-07-16 21:39:47 +0530

    SpringApplication - initial project setup

---
+++ 01-spring-boot-features-spring-application/src/main/java/com/ds2/tutorials/Application.java
@@ -0,0 +1,13 @@
+package com.ds2.tutorials;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
